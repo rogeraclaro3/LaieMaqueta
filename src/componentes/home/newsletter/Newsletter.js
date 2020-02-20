d0593886710e1f937764578b1c0b05f5e886eeeb
Roger Masellas <roger@lamosca.com>
2020-02-21 00:10:24 +0100

    form newsletter & deleted pages

--- src/componentes/home/newsletter/Newsletter.js
+++ src/componentes/home/newsletter/Newsletter.js
@@ -7,14 +7,19 @@ class Tarjeta extends React.Component {
     return (
         <div className="container-fluid separav60_pad separab60_pad tarjeta-fons">
 					<div className="container">
-							<div className="row">
-									<div className="col-lg-12 centrat">
-										<span className="titol">Subscriu-te al nostre newsletter</span>
-										<span className="text">
-											<p>estigues al corrent de totes les novetats i aconsegueix un 5% de descompte en la teva primera compra</p>
-										</span>
-									</div>
+						<div className="row">
+							<div className="col-lg-12 centrat">
+								<span className="titol">Subscriu-te al nostre newsletter</span>
+								<span className="text">
+									<p>estigues al corrent de totes les novetats i aconsegueix un 5% de descompte en la teva primera compra</p>
+								</span>
 							</div>
+						</div>
+						<form className="row mb-5">
+							<div className="col-lg-12 centrat">
+								<input type="email" className="form-control" id="Email" value="Email" required /><button type="submit" className="btn btn-primary">Enviar</button>
+							</div>
+						</form>
 					</div>
 				</div>
     )
--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter as Router, Route, Switch} from 'react-router-dom';
-
 import './index.css';
-
 import * as serviceWorker from './serviceWorker';
-
-//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-//import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-//import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Home from './componentes/home/Home';
-import Nosotros from './componentes/nosotros/Nosotros';
-import Servicios from './componentes/servicios/Servicios';
-import Contacto from './componentes/contacto/Contacto';
+//import Nosotros from './componentes/nosotros/Nosotros';
+//import Servicios from './componentes/servicios/Servicios';
+//import Contacto from './componentes/contacto/Contacto';
 
 ReactDOM.render (
   <Router>
@@ -24,9 +15,9 @@ ReactDOM.render (
       <Switch>
         {/* Paginas */}
         <Route exact path="/" component={Home} />
-        <Route path="/nosotros" component={Nosotros} />
-        <Route path="/servicios" component={Servicios} />
-        <Route path="/contacto" component={Contacto} />
+        {/* <Route path="/nosotros" component={Nosotros} /> */}
+        {/* <Route path="/servicios" component={Servicios} /> */}
+        {/* <Route path="/contacto" component={Contacto} /> */}
       </Switch>
     </div>
   </Router>,
